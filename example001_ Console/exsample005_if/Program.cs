9ff47b18ca1e47ec9b66528b89a750174bea06b6
Olga <kirillovaolya@gmail.com>
2022-08-22 12:34:49 +0400

    Добавили 005

---
+++ example001_ Console/exsample005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Это маша");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
